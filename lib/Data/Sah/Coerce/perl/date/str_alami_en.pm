6ad5a27170d430e5e8c73eb595d7da63cc631d7d
perlancar (@pc-office) <perlancar@gmail.com>
2016-05-20 16:53:37 +0700

    Add rules: str_alami_en, str_alami_id

---
+++ lib/Data/Sah/Coerce/perl/date/str_alami_en.pm
@@ -0,0 +1,45 @@
+package Data::Sah::Coerce::perl::date::str_alami_en;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+sub meta {
+    +{
+        enable_by_default => 0,
+        might_die => 1,
+        prio => 60, # a bit lower than normal
+    };
+}
+
+sub coerce {
+    my %args = @_;
+
+    my $dt = $args{data_term};
+
+    my $res = {};
+
+    unless ($args{coerce_to} eq 'DateTime') {
+        die "To use this coercion rule, you must coerce your date to DateTime (e.g. by adding this attribute 'x.perl.coerce_to' => 'DateTime' to your schema)";
+    }
+
+    $res->{expr_match} = "!ref($dt)";
+    $res->{modules}{"DateTime::Format::Alami::EN"} //= 0;
+    $res->{expr_coerce} = "DateTime::Format::Alami::EN->new->parse_datetime($dt)";
+
+    $res;
+}
+
+1;
+# ABSTRACT: Coerce date from string parsed by DateTime::Format::Alami::EN
+
+=for Pod::Coverage ^(meta|coerce)$
+
+=head1 DESCRIPTION
+
+The rule is not enabled by default. You can enable it in a schema using e.g.:
+
+ ["array*", of=>"date", "x.perl.coerce_to"=>"DateTime", "x.perl.coerce_from"=>["str_alami_en"]]
---
+++ t/perl-str_alami_id.t
@@ -0,0 +1,25 @@
+#!perl
+
+use 5.010001;
+use strict;
+use warnings;
+
+use Data::Sah::Coerce qw(gen_coercer);
+use Test::More 0.98;
+use Test::Needs;
+
+subtest "coerce_to=DateTime" => sub {
+    test_needs "DateTime";
+    test_needs "DateTime::Format::Alami::ID";
+
+    my $c = gen_coercer(type=>"date", coerce_to=>"DateTime", coerce_from=>["str_alami_id"]);
+
+    # uncoerced
+    is_deeply($c->({}), {}, "uncoerced");
+
+    my $d = $c->("19 mei 2016");
+    is(ref($d), 'DateTime');
+    is($d->ymd, "2016-05-19");
+};
+
+done_testing;
